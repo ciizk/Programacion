18a3e298553c471f0424a08e8ee57eee4abea1b4
faiv <faindval@epsg.upv.es>
2026-02-10 17:06:01 +0100

    empezando integral

---
+++ javaScript/asignatura/prácticas/p1/integralTrozos.js
@@ -0,0 +1,6 @@
+/*--------------------------------------------------------------------------------------------
+Fichero: porTres.js
+Autor: Francisco Indriago
+Fecha: 11/02/2026
+Descripción: Función que recibe un número real y lo multiplica por tres.
+--------------------------------------------------------------------------------------------*/
